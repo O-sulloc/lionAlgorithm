89720b359018fc8174079d3e5cc98ccabc04d575
O-sulloc <bujjaf@gmail.com>
2022-10-19 10:47:24 +0900

    stack study pop test

--- src/main/java/week5/day3/algo/StackStudy.java
+++ src/main/java/week5/day3/algo/StackStudy.java
@@ -12,16 +12,25 @@ public class StackStudy {
         this.arr = new int[size];
     }
 
+    public int pop() {
+        //pop하면 배열 젤 위에 있는걸 꺼내줘 그리고 삭제해줘
+
+        int value = this.arr[this.pointer-1]; //포인터가 가르키는 위치. 거기가 꺼내고자 하는 array 인덱스의 위치. 꺼낸 다음에 value 변수에 넣어줘
+        this.pointer--; //포인트는 감소시켜줘. 이제 거기에 데이터 없으니까?
+        //삭제는 언제하죠?
+
+        return value; //꺼낸 데이터 리턴
+    }
+
     public void push(int value) {
         //push하면 Value를 넣어줘. 어레이 배열에
 
         this.arr[this.pointer] = value;
-        this.pointer++; //하나 진행될때마다
+        this.pointer++; //하나 진행될때마다 ++로 증가해줘
     }
 
-    public int[] getArr(){
+    public int[] getArr() {
         //private이라 테스트에서 접근이 안되니까 get으로 불러올 수 있게 만들어줌.
-
         return arr;
     }
 }
--- src/test/java/week5/day3/algo/StackStudyTest.java
+++ src/test/java/week5/day3/algo/StackStudyTest.java
@@ -1,13 +1,14 @@
 package week5.day3.algo;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackStudyTest {
 
-    @Test
+    //@Test
     void pushTest() {
         StackStudy ss = new StackStudy();
         ss.push(10);
@@ -17,5 +18,19 @@ class StackStudyTest {
 
         Assertions.assertEquals(10, arr[0]);
         Assertions.assertEquals(20, arr[1]);
+    }
+
+    @Test
+    @DisplayName("push pop 둘 다 동시에 테스트")
+    void pushAndPop(){
+        StackStudy ss = new StackStudy();
+        ss.push(10);
+        ss.push(20);
+
+        Assertions.assertEquals(20,ss.pop());
+        Assertions.assertEquals(10,ss.pop());
+
+        ss.push(30);
+        Assertions.assertEquals(30,ss.pop());
     }
 }
