fa3f850e63d777b484327461cae95ebd8cca42be
O-sulloc <bujjaf@gmail.com>
2022-10-19 10:21:29 +0900

    stack study push test

--- src/main/java/week5/day3/algo/StackStudy.java
+++ src/main/java/week5/day3/algo/StackStudy.java
@@ -1,2 +1,27 @@
-package week5.day3.algo;public class StackStudy {
+package week5.day3.algo;
+
+public class StackStudy {
+    private int[] arr = new int[100000];
+    private int pointer = 0; //포인터로 지금상태?를 확인할 수 있음.
+
+    public StackStudy() {
+
+    }
+
+    public StackStudy(int size) {
+        this.arr = new int[size];
+    }
+
+    public void push(int value) {
+        //push하면 Value를 넣어줘. 어레이 배열에
+
+        this.arr[this.pointer] = value;
+        this.pointer++; //하나 진행될때마다
+    }
+
+    public int[] getArr(){
+        //private이라 테스트에서 접근이 안되니까 get으로 불러올 수 있게 만들어줌.
+
+        return arr;
+    }
 }
--- src/test/java/week5/day3/algo/StackStudyTest.java
+++ src/test/java/week5/day3/algo/StackStudyTest.java
@@ -1,4 +1,21 @@
+package week5.day3.algo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StackStudyTest {
-  
+
+    @Test
+    void pushTest() {
+        StackStudy ss = new StackStudy();
+        ss.push(10);
+        ss.push(20);
+
+        int[] arr = ss.getArr();
+
+        Assertions.assertEquals(10, arr[0]);
+        Assertions.assertEquals(20, arr[1]);
+    }
 }
