c62f8e969de94cf6190364e2c625455604d2b375
O-sulloc <bujjaf@gmail.com>
2022-10-31 10:17:09 +0900

    모의고사 (완전 탐색)

--- src/main/java/week7/day1/BruteForce.java
+++ src/main/java/week7/day1/BruteForce.java
@@ -1,11 +1,13 @@
 package week7.day1;
 
+import java.util.Arrays;
+
 public class BruteForce {
     public int solution(int[] answers) {
         //int[] 받아서 Int[] 리턴하는 메서드
 
         /*test case
-        문제 1. 답지[1,2,3,4,5]	가장 많이 맞춘(5) 학생 [1]번 수포자 학생
+        문제 1. 답지[1,2,3,4,5]	가장 많이 맞춘(5문제) 학생 [1]번 수포자 학생
         문제 2. 답지[1,3,2,4,2]	[1,2,3]번 학생이 각각 2문제씩 맞음
          */
 
@@ -18,20 +20,26 @@ public class BruteForce {
             1트: 0 / 2트: 2
          */
 
-        int[] a = {1,2,3,4,5};
-        int[] b = {2,1,2,3,2};
-        int[] c = {3,3,1,1,2};
+        int[] stu1 = {1,2,3,4,5};
+        int[] stu2 = {2,1,2,3,2};
+        int[] stu3 = {3,3,1,1,2};
+        int[] scores = {0,0,0};
 
-        int firstCount = 0; //첫번째 문제 답안체크해서 맞은 문제수 카운트
+        int cnt = 0; //첫번째 문제 답안체크해서 맞은 문제수 카운트
 
-        for (int i = 0; i <5 ; i++) {
-            if (answers[i] == a[i]){
-                firstCount++;
+        for (int j = 0; j < 3; j++) {
+            //학생 3명 반복하고 싶은데.........
+
+            for (int i = 0; i < answers.length; i++) {
+                if (answers[i] == stu1[i]) {
+                    cnt++;
+                }
+                scores[0] = cnt;
             }
         }
 
-
-        return firstCount;
+        System.out.println(Arrays.toString(scores));
+        return cnt;
     }
 
     public static void main(String[] args) {
