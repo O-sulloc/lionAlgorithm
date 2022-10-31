2466c5ca5352514fa80b31214634e210ce6be82b
O-sulloc <bujjaf@gmail.com>
2022-11-01 01:38:06 +0900

    모의고사 (완전 탐색)

--- src/main/java/week7/day1/BruteForce.java
+++ src/main/java/week7/day1/BruteForce.java
@@ -1,9 +1,10 @@
 package week7.day1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteForce {
-    public int solution(int[] answers) {
+    public int[] solution(int[] answers) {
         //int[] 받아서 Int[] 리턴하는 메서드
 
         /*test case
@@ -20,32 +21,41 @@ public class BruteForce {
             1트: 0 / 2트: 2
          */
 
-        int[] stu1 = {1,2,3,4,5};
-        int[] stu2 = {2,1,2,3,2};
-        int[] stu3 = {3,3,1,1,2};
-        int[] scores = {0,0,0};
+        int[] stu1 = {1, 2, 3, 4, 5};
+        int[] stu2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] stu3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] scores = {0, 0, 0};
+        int[] answer = {};
 
         int cnt = 0; //첫번째 문제 답안체크해서 맞은 문제수 카운트
 
-        for (int j = 0; j < 3; j++) {
-            //학생 3명 반복하고 싶은데.........
-
-            for (int i = 0; i < answers.length; i++) {
-                if (answers[i] == stu1[i]) {
-                    cnt++;
-                }
-                scores[0] = cnt;
-            }
+        for (int i = 0; i < answers.length; i++) {
+            if (stu1[i % stu1.length] == answers[i]) scores[0]++;
+            if (stu2[i % stu1.length] == answers[i]) scores[1]++;
+            if (stu3[i % stu1.length] == answers[i]) scores[2]++;
         }
 
-        System.out.println(Arrays.toString(scores));
-        return cnt;
+        int maxScore = Math.max(Math.max(scores[0], scores[1]), scores[2]);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (maxScore == scores[0]) list.add(1);
+        if (maxScore == scores[1]) list.add(2);
+        if (maxScore == scores[2]) list.add(3);
+
+        answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        System.out.println(Arrays.toString(answer));
+        return answer;
     }
 
     public static void main(String[] args) {
-        int[] firstAnswer = {1, 2, 3, 4, 5};
+        //int[] firstAnswer = {1, 2, 3, 4, 5};
+        int[] secondAnswer = {1, 3, 2, 4, 2};
 
         BruteForce bf = new BruteForce();
-        bf.solution(firstAnswer);
+        bf.solution(secondAnswer);
     }
 }
