78b37c01d1a229667c611dfaca838db44a0e819a
O-sulloc <bujjaf@gmail.com>
2022-11-07 09:38:36 +0900

    Harshad Number

--- src/main/java/week8/day1/Harshad.java
+++ src/main/java/week8/day1/Harshad.java
@@ -2,20 +2,27 @@ package week8.day1;
 
 public class Harshad {
     public boolean solution(int x) {
-        boolean answer = true;
+        boolean answer = false;
+        int sum = 0;
+        int ori = x;
+
+        //자릿수 합 구하기
+        while (x > 0) {
+            sum += x % 10;
+            //System.out.println(x % 10);
+            x = x / 10;
+        }
+
+        //하샤드 수인지 판별
+        if (ori % sum == 0) {
+            answer = true;
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
-        int x = 687;
-        int sum = 0;
-        for (int i = 0; i < 3; i++) {
-            sum += x % 10;
-            System.out.println(x % 10);
-            x = x / 10;
-        }
-        System.out.println(sum);
-
-        System.out.println(x/sum);
+        Harshad harshad = new Harshad();
+        harshad.solution(687);
     }
 }
