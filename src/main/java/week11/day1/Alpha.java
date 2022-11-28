e00661943485706f8ee90b15b28a2076ccb43069
O-sulloc <bujjaf@gmail.com>
2022-11-28 09:44:58 +0900

    print a~z

--- src/main/java/week11/day1/Alpha.java
+++ src/main/java/week11/day1/Alpha.java
@@ -4,19 +4,19 @@ public class Alpha {
     //알파벳 두 개를 조합해서 나올 수 있는 모든 경우의 수 출력하기 AA, AB, ...
     //아스키 65~90까지가 A~Z
 
-    public void print(int n) {
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            for (char ch2 = 'A'; ch2 <= 'Z'; ch2++) {
-                System.out.printf("%s%s\n", ch, ch2);
-            }
-        }
+    public void print(char ch) {
 
+        if(ch > 'Z'){
+            return;
+        }
+        System.out.println(ch);
+        print((char) (ch+1));
     }
 
     public static void main(String[] args) {
         Alpha alpha = new Alpha();
-        int n = 2; //칸이 n개인 경우 (2개인 경우 AA,AB ...) (3개인 경우 AAA, ...)
-        alpha.print(n);
+        //int n = 2; //칸이 n개인 경우 (2개인 경우 AA,AB ...) (3개인 경우 AAA, ...)
+        alpha.print('A');
 
         /* 내가 푼 방법..
         char ch = 0;
