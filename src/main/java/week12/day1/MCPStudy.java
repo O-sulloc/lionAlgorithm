28d50025d0e95ca120edee3a3f1e223afc4abba1
O-sulloc <bujjaf@gmail.com>
2022-12-12 10:40:03 +0900

    2. 1행만 넣기

--- src/main/java/week12/day1/MCPStudy.java
+++ src/main/java/week12/day1/MCPStudy.java
@@ -10,8 +10,13 @@ public class MCPStudy {
         // 2. (0,0) 채우기
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if(i == 0 && j == 0){
+                if (i == 0 && j == 0) {
+                    //(0,0) 넣는 로직
                     dp[i][j] = matrix[i][j];
+                } else if (i == 0 && j > 0) {
+                    //첫행만 있을 경우 (0,0), (0,1), (0,2)
+                    dp[i][j] = dp[i][j - 1] + matrix[i][j];
+
                 }
             }
         }
