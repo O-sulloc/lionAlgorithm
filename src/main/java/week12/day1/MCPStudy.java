5adf5dbd318203633ca3f10b3c61fa32086baadb
O-sulloc <bujjaf@gmail.com>
2022-12-12 10:31:00 +0900

    2. (0,0) 넣기

--- src/main/java/week12/day1/MCPStudy.java
+++ src/main/java/week12/day1/MCPStudy.java
@@ -7,11 +7,18 @@ public class MCPStudy {
         // 1. 3*3 DP 만들기
         int[][] dp = new int[matrix.length][matrix[0].length];
 
+        // 2. (0,0) 채우기
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(i == 0 && j == 0){
+                    dp[i][j] = matrix[i][j];
+                }
+            }
+        }
+
         for (int i = 0; i < matrix.length; i++) {
             System.out.println(Arrays.toString(dp[i]));
         }
-
-        //
     }
 
     public static void main(String[] args) {
