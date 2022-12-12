0f900e3b1ab2163299238df483f8191bf426598d
Jeonghyeon <bujjaf@gmail.com>
2022-12-12 09:36:47 +0900

    1. 3*3 DP 선언

---
+++ src/main/java/week12/day1/MCPStudy.java
@@ -0,0 +1,22 @@
+package week12.day1;
+
+import java.util.Arrays;
+
+public class MCPStudy {
+    public static void sol(int[][] matrix) {
+        // 1. 3*3 DP 만들기
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        // 2. 첫째칸만 있는 경우
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 2}, {4, 6, 2}, {1, 2, 4}};
+
+        sol(matrix);
+    }
+}
