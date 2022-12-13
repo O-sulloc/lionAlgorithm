0924ca3faba632bc572fb10e037cf3db6100309a
O-sulloc <bujjaf@gmail.com>
2022-12-13 10:41:14 +0900

    2. 동전이 두개 있는 경우

--- src/main/java/week12/day2/Optimal.java
+++ src/main/java/week12/day2/Optimal.java
@@ -28,8 +28,17 @@ public class Optimal {
             //(0,0), (1,1) ... 대각선으로 넣기
         }
 
-        for (int i = 0; i < coins.length; i++) {
-            System.out.println(dp[i][i].toString());
+        // 동전이 두 개 있는 경우, dp에 값 넣기
+        for (int i = 0; i < coins.length - 1; i++) {
+            System.out.printf("%d %d %d %d\n", i, i + 1, coins[i], coins[i + 1]);
+            int left = Math.max(coins[i], coins[i + 1]); //둘 중 큰 수를 left에
+            int right = Math.min(coins[i], coins[i + 1]); //작은 수를 right에
+            dp[i][i + 1] = new Optimal(left, right);
+            System.out.println(dp[i][i + 1].toString());
         }
+
+//        for (int i = 0; i < coins.length; i++) {
+//            System.out.println(dp[i][i].toString());
+//        }
     }
 }
